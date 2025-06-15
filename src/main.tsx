a8605fc12636fea962c173b46dc61827cb9376ff
duyluongg <duyluongg1910@gmail.com>
2025-06-15 10:18:44 +0700

    fix role authoraztion

--- src/main.tsx
+++ src/main.tsx
@@ -13,12 +13,15 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
+import { AuthProvider } from './services/AuthContext'; // 👈 IMPORT AuthProvider!
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </Provider>
   </React.StrictMode>
 );

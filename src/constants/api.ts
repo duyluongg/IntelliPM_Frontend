341da6971a2ebff4687e9b52946d9f0bb9d1ac99
khangper <89836419+khangper@users.noreply.github.com>
2025-08-18 12:43:08 +0700

    Create meeting with systemConfiguration

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
-// export const API_BASE_URL = 'https://localhost:7128/api/';
+export const API_BASE_URL = 'https://localhost:7128/api/';
 
-export const API_BASE_URL = 'https://intellipm-fpt-g6drgmbga8fcbubw.southeastasia-01.azurewebsites.net/api/';
+// export const API_BASE_URL = 'https://intellipm-fpt-g6drgmbga8fcbubw.southeastasia-01.azurewebsites.net/api/';
 
